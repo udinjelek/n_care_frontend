e18ec631011596cd372943d2354939a76226354e
udin jelek <udin@jelek.com>
2024-05-16 03:40:42 +0700

    trial api not using port

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,5 @@
 // export const SOCKET_IO_URL = 'http://127.0.0.1:5001';
 // export const API_BASE_URL = 'https://192.168.31.117:5001/api/';
 // export const SOCKET_IO_URL = 'https://192.168.31.117:5001';
-export const API_BASE_URL = 'https://care-chat-api.polaris.my.id:5001/api/';
+export const API_BASE_URL = 'https://care-chat-api.polaris.my.id/api/';
 export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id:5001';
