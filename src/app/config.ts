5a5008ca561f7cebc8519579bd2f1bfbe42e9571
udin jelek <udin@jelek.com>
2024-05-15 21:15:04 +0700

    add port 5001

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,4 @@
 // export const API_BASE_URL = 'http://127.0.0.1:5001/api/';
-export const API_BASE_URL = 'https://care-chat-api.polaris.my.id/api/';
-export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id';
+// export const SOCKET_IO_URL = 'http://127.0.0.1:5001';
+export const API_BASE_URL = 'https://care-chat-api.polaris.my.id:5001/api/';
+export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id:5001';
