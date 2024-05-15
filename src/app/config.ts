cb54ce074237fdc5553a052d59e8b6b8d426df1c
udin jelek <udin@jelek.com>
2024-05-15 12:24:28 +0700

    edit out dist\n-care-chat-app

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = 'http://127.0.0.1:5001/api/';
-export const API_BASE_URL = 'http://care-chat-api.polaris.my.id/api/';
-export const SOCKET_IO_URL = 'http://care-chat-api.polaris.my.id';
+export const API_BASE_URL = 'https://care-chat-api.polaris.my.id/api/';
+export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id';
