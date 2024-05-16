ef5d230a88b98a9d07e429314bf40cf119aee709
udin jelek <udin@jelek.com>
2024-05-16 07:37:46 +0700

    test socket not using port

--- src/app/config.ts
+++ src/app/config.ts
@@ -3,4 +3,4 @@
 // export const API_BASE_URL = 'https://192.168.31.117:5001/api/';
 // export const SOCKET_IO_URL = 'https://192.168.31.117:5001';
 export const API_BASE_URL = 'https://care-chat-api.polaris.my.id/api/';
-export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id:5001';
+export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id';
