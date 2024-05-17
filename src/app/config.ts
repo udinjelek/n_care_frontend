09c2e27f1808790e17f5a953c666348d2fc4c279
udin jelek <udin@jelek.com>
2024-05-17 11:33:42 +0700

    beta target api

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,5 +2,7 @@
 // export const SOCKET_IO_URL = 'http://127.0.0.1:5001';
 // export const API_BASE_URL = 'https://192.168.31.117:5001/api/';
 // export const SOCKET_IO_URL = 'https://192.168.31.117:5001';
-export const API_BASE_URL = 'https://care-chat-api.polaris.my.id/api/';
-export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id';
+// export const API_BASE_URL = 'https://care-chat-api.polaris.my.id/api/';
+// export const SOCKET_IO_URL = 'https://care-chat-api.polaris.my.id';
+export const API_BASE_URL = 'https://care-chat-api.betablog.my.id/api/';
+export const SOCKET_IO_URL = 'https://care-chat-api.betablog.my.id';
