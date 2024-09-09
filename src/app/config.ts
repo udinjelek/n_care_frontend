f497288af47a981f0aebb5475e656e07b9947765
udin jelek <udin@jelek.com>
2024-09-09 17:09:18 +0700

    update 50001 port no need to say

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = 'http://127.0.0.1:50001/api/'; // test for local
 // export const SOCKET_IO_URL = 'http://127.0.0.1:50001'; // test for local
-export const API_BASE_URL = 'http://care-chat-api.polaris.my.id:50001/api/'; // test for production
-export const SOCKET_IO_URL = 'http://care-chat-api.polaris.my.id:50001'; // test for production
+export const API_BASE_URL = 'http://care-chat-api.polaris.my.id/api/'; // test for production
+export const SOCKET_IO_URL = 'http://care-chat-api.polaris.my.id'; // test for production
